9be84bd0d63d50c94e90d92061dc695f3dc81dd7
Mikhail Lanets <theoilside@gmail.com>
2024-05-19 21:43:59 +0500

    Added: ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,17 @@ import { RouterProvider } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import router from "./routes/browser-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-          <ChakraProvider>
-            <RouterProvider router={router} />
-          </ChakraProvider>
-        </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
